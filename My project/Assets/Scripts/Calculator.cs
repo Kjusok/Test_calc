c7eb0de2a4a2f229fcf6c6cff271a4d183b89117
Kjusok <103447181+Kjusok@users.noreply.github.com>
2022-04-25 23:42:01 +0300

    Added buttons for numbers, signs, reset, Log2, Log10 and dots.
    Добавились кнопки 1-9 и ","
    Добавилась кнопка обнуления вводимого значения "C" и кнопка изменения положительного числа на отрицательный (+/-)
    Заменены кнопки  мин и мак на кнопки Логарифм: десятичный и двоичный

--- My project/Assets/Scripts/Calculator.cs
+++ My project/Assets/Scripts/Calculator.cs
@@ -1,11 +1,10 @@
-
+  
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class Calculator : MonoBehaviour
 {
-  
     public InputField FirstValInput;
     public InputField SecValInput;
 
@@ -13,58 +12,259 @@ public class Calculator : MonoBehaviour
     public float secondValue;
 
     public string operation;
-
-
+    public bool InputSecondValue;
+    
     public void Start()
     {
-        
         FirstValInput.text = "0";
-        
+        InputSecondValue = false;
     }
 
-    
+    // operations
+    public void CE ()
+    {
+        FirstValInput.text = "0";
+        operation = null;
+    }
     public void Plus()
     {
        float.TryParse(FirstValInput.text, out firstValue);
        operation = "+";
+        InputSecondValue = true;
     }
     public void Minus()
     {
         float.TryParse(FirstValInput.text, out firstValue);
         operation = "-";
+        InputSecondValue = true;
     }
     public void Multi()
     {
         float.TryParse(FirstValInput.text, out firstValue);
         operation = "*";
+        InputSecondValue = true;
     }
     public void Div()
     {
         float.TryParse(FirstValInput.text, out firstValue);
         operation = "/";
+        InputSecondValue = true;
     }
     public void Pow()
     {
         float.TryParse(FirstValInput.text, out firstValue);
         operation = "^";
+        InputSecondValue = true;
     }
-    public void Max()
+    public void Log10()
     {
         float.TryParse(FirstValInput.text, out firstValue);
-        operation = ">";
+        float Log10 = Mathf.Log10(firstValue);
+        FirstValInput.text = "" + Log10 + "";
     }
-    public void Min()
+    public void Log2()
     {
         float.TryParse(FirstValInput.text, out firstValue);
-        operation = "<";
-
+        float Log2 = Mathf.Log(firstValue,2);
+        FirstValInput.text = "" + Log2 + "";
     }
-
+    // numbers
+    public void one()
+    {
+            if (FirstValInput.text == "0")
+            {
+                FirstValInput.text = "1";
+            }
+            else
+            {
+                FirstValInput.text += "1";
+            }
+            if (InputSecondValue) 
+            {
+            InputSecondValue = false;
+            FirstValInput.text = "1";
+            }
+    }
+    public void two()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "2";
+        }
+        else
+        {
+            FirstValInput.text += "2";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "2";
+        }
+    }
+    public void three()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "3";
+        }
+        else
+        {
+            FirstValInput.text += "3";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "3";
+        }
+    }
+    public void Four()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "4";
+        }
+        else
+        {
+            FirstValInput.text += "4";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "4";
+        }
+    }
+    public void Five()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "5";
+        }
+        else
+        {
+            FirstValInput.text += "5";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "5";
+        }
+    }
+    public void Six()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "6";
+        }
+        else
+        {
+            FirstValInput.text += "6";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "6";
+        }
+    }
+    public void Seven()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "7";
+        }
+        else
+        {
+            FirstValInput.text += "7";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "7";
+        }
+    }
+    public void Eigth()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "8";
+        }
+        else
+        {
+            FirstValInput.text += "8";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "8";
+        }
+    }
+    public void Nine()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "9";
+        }
+        else
+        {
+            FirstValInput.text += "9";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "9";
+        }
+    }
+    public void Zero()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "0";
+        }
+        else
+        {
+            FirstValInput.text += "0";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "0";
+        }
+    }
+    public void Point()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "0,";
+        }
+        else
+        {
+            FirstValInput.text += ",";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = ",";
+        }
+    }
+    public void PlusOrMinus ()
+    {
+        if (FirstValInput.text == "0")
+        {
+            FirstValInput.text = "-";
+        }
+        else
+        {
+            FirstValInput.text += "";
+        }
+        if (InputSecondValue)
+        {
+            InputSecondValue = false;
+            FirstValInput.text = "-";
+        }
+    }
+    //results
     public void GetAnswer()
         {
-
             float.TryParse(SecValInput.text, out secondValue);
-            if (firstValue != 0 && secondValue != 0 && !string.IsNullOrEmpty(operation)) ;
             {
                 switch (operation)
                 {
@@ -72,7 +272,7 @@ public class Calculator : MonoBehaviour
                         float sum = firstValue + secondValue;
                         FirstValInput.text = "" + sum + "";
                         break;
-                    case "-":
+                case "-":
                         float DifferenceOfValues = firstValue - secondValue;
                         FirstValInput.text = "" + DifferenceOfValues + "";
                         break;
@@ -92,32 +292,9 @@ public class Calculator : MonoBehaviour
                         float pow = Mathf.Pow(firstValue, secondValue);
                         FirstValInput.text = "" + pow + "";
                     break;
-                case ">":
-                    if (firstValue >= secondValue)
-                    {
-                        FirstValInput.text = "" + firstValue + "";
-                    }
-                    else FirstValInput.text = "" + secondValue + "";
-                    break;
-                case "<":
-                    if (firstValue <= secondValue)
-                    {
-                        FirstValInput.text = "" + firstValue + "";
-                    }
-                    else FirstValInput.text = "" + secondValue + "";
-                    break;
-
             }
-
-
             }
-
-
-
-
         } 
-      
-
    
     }
  
