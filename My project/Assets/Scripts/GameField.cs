9b2ad8af594f78fed8cd88a50a451256f370cd96
Kjusok <103447181+Kjusok@users.noreply.github.com>
2022-05-25 10:33:29 +0300

    fixed the timer and saving the best result
    fixed the timer and saving the best result, also fixed, enabling / disabling input fields and buttons

--- My project/Assets/Scripts/GameField.cs
+++ My project/Assets/Scripts/GameField.cs
@@ -6,33 +6,48 @@ using UnityEngine.SceneManagement;
 public class GameField : MonoBehaviour
 {
     public InputField GetAnswer;
+    public Button StartTimer;
     public Image TimeBar;
     public Text Timer;
     public Text Results;
     public Text BestTime;
+    public Text Task;
 
     public float Answer;
     public float GameTime;
 
+    private float _bestTime;
+
     public bool StopTimer;
-    public bool MemoryTimer;
 
     void Start()
     {
-        TimeBar =TimeBar.gameObject.GetComponent<Image>();
-        StopTimer = true;
-        MemoryTimer = false;
+        TimeBar = TimeBar.gameObject.GetComponent<Image>();
+        StopTimer = false;
+        GetAnswer.interactable = false;
         GameTime = 180;
-        Results.text = "œŒ≈’¿À»!";
+        Results.text = "√Œ“Œ¬?";
+        BestTime.text = "0:00";
+        Timer.text = "3:00";
+        Task.text = "";
     }
-    void Update()
+    public void ButtonStartTimer()
     {
-        //Timer Stop
-        if (GameTime <= 0 ^ Answer == 13)
+        StopTimer = true;
+        Results.text = "œŒ≈’¿À»!";
+        Task.text = "(-18,2+45/0,5+(4^3 *0,3))/7";
+        StartTimer.interactable = false;
+        GetAnswer.interactable = true;
+    }
+    private void TimerStop()
+    {
+        if (GameTime <= 0 || Answer == 13)
         {
             StopTimer = false;
-                    }
-        //Timer
+        }
+    }
+    private void TimerStart()
+    {
         if (StopTimer == true)
         {
             GameTime -= Time.deltaTime;
@@ -41,14 +56,18 @@ public class GameField : MonoBehaviour
             int seconds = Mathf.FloorToInt(GameTime - minutes * 60f);
             string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
             Timer.text = textTime;
-            BestTime.text = PlayerPrefs.GetString("BestTime");
         }
-        // Timer scale
+    }
+    private void TimerScale()
+
+    {
         if (GameTime > 0)
         {
             TimeBar.fillAmount = GameTime / 180;
         }
-        // Press Enter to Results and save best time
+    }
+    private void PressEnter()
+    {
         if (Input.GetKeyDown(KeyCode.Return))
         {
             float.TryParse(GetAnswer.text, out Answer);
@@ -59,14 +78,42 @@ public class GameField : MonoBehaviour
             else
             {
                 Results.text = "¬—≈ ¬≈–ÕŒ!";
-                BestTime.text = Timer.text;
-                PlayerPrefs.SetString("BestTime", BestTime.text);
+                if (GameTime > _bestTime)
+                {
+                    _bestTime = GameTime;
+                    BestTime.text = Timer.text;
+                }
             }
         }
     }
+    private void CheckTimeAndBestTime()
+    {
+        if (GameTime > _bestTime)
+        {
+            int minutes = Mathf.FloorToInt(_bestTime / 60);
+            int seconds = Mathf.FloorToInt(_bestTime - minutes * 60f);
+            BestTime.text = _bestTime.ToString(string.Format("{0:0}:{1:00}", minutes, seconds));
+        }
+    }
+    void Update()
+    {
+        TimerStop();
+        TimerStart();
+        TimerScale();
+        PressEnter();
+        CheckTimeAndBestTime();
+    }
     public void RestartGame()
     {
-        SceneManager.LoadScene("Game");
+        StartTimer.interactable = true;
+        GetAnswer.interactable = false;
+        StopTimer = false;
+        GameTime = 180;
+        Answer = 0;
+        GetAnswer.text = "";
+        Results.text = "√Œ“Œ¬?";
+        Timer.text = "3:00";
+        Task.text = "";
     }
-}
 
+}
